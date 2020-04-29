2f90eb02bc4ad031c4019a2cd6f5d8ab44b4083f
ketanyadav10 <kyadav073@gmail.com>
2020-04-29 10:48:08 +0530

    Changed codes for exception

--- src/main/java/com/evolent/contactmgmt/exceptions/ExceptionControllerAdvice.java
+++ src/main/java/com/evolent/contactmgmt/exceptions/ExceptionControllerAdvice.java
@@ -2,6 +2,7 @@ package com.evolent.contactmgmt.exceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -17,7 +18,16 @@ public class ExceptionControllerAdvice {
 	@ExceptionHandler(NoSuchContactException.class)
 	public ResponseEntity<ErrorMessage> exceptionHandler2(NoSuchContactException ex) {
 		 ErrorMessage error = new ErrorMessage();
-	        error.setErrorCode(HttpStatus.BAD_GATEWAY.value());
+	        error.setErrorCode(HttpStatus.NOT_FOUND.value());
+	        error.setMessage(ex.getMessage());
+	        return new ResponseEntity<>(error, HttpStatus.OK);
+		 
+	}
+	 
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<ErrorMessage> handleMethodArgumentNotValid(MethodArgumentNotValidException ex) {
+		 ErrorMessage error = new ErrorMessage();
+	        error.setErrorCode(HttpStatus.BAD_REQUEST.value());
 	        error.setMessage(ex.getMessage());
 	        return new ResponseEntity<>(error, HttpStatus.OK);
 		 
@@ -25,7 +35,7 @@ public class ExceptionControllerAdvice {
 	@ExceptionHandler(DuplicateContactException.class)
 	public ResponseEntity<ErrorMessage> exceptionHandler2(DuplicateContactException ex) {
 		 ErrorMessage error = new ErrorMessage();
-	        error.setErrorCode(HttpStatus.BAD_GATEWAY.value());
+	        error.setErrorCode(HttpStatus.CONFLICT.value());
 	        error.setMessage(ex.getMessage());
 	        return new ResponseEntity<>(error, HttpStatus.OK);
 		 
